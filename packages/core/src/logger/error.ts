1333e9000ec5d939d8b100a11c5bb73a37ede813
ysh <yangshuhao@qq.com>
2024-04-28 19:11:31 +0800

    初始化架构

--- packages/core/src/logger/error.ts
+++ packages/core/src/logger/error.ts
@@ -1,7 +1,8 @@
 import { packageName } from './enum.ts'
 
 const error = (...arg: unknown[]) => {
-  console.error(packageName, ...arg)
+  throw new Error(`${packageName}, ${arg.join(', ')}`)
+  // console.error(packageName, ...arg)
 }
 
 export default error
--- packages/core/src/player/index.ts
+++ packages/core/src/player/index.ts
@@ -1,6 +1,6 @@
 import Lrc from './lrc'
-import Logger from "../logger";
-import {LyricsLine} from "../types/type";
+import Logger from '../logger'
+import { LyricsLine, Yrc } from '../types/type'
 
 interface Core {
   animationFrameId: number | null
@@ -27,7 +27,7 @@ class Player {
     // 绑定事件处理方法到类的实例上
   }
   play = async () => {
-    if(this.isPlaying) {
+    if (this.isPlaying) {
       return
     }
     try {
@@ -41,7 +41,7 @@ class Player {
     }
   }
   pause = () => {
-    if(!this.isPlaying) {
+    if (!this.isPlaying) {
       return
     }
     try {
@@ -57,13 +57,16 @@ class Player {
     this.audio.volume = volume
   }
   updateTime = (time: number, index?: number) => {
+    if (!this.audio.src) {
+      return
+    }
     this.audio.currentTime = time
-    if(index) {
+    if (index) {
       this.index = index
     } else {
       this.index = this.getCurrentLrcLine().index
     }
-    this.curAnimations.forEach(animation => {
+    this.curAnimations.forEach((animation) => {
       animation.cancel()
     })
     console.log('this.index', this.index)
@@ -71,7 +74,9 @@ class Player {
   /* 更新url, 更新歌词，从而使其重新渲染 */
   updateAudioUrl = (url: string, lrc: string) => {
     // 移除旧的事件监听器
-    this.audio.removeEventListener("canplaythrough", () => this.onCanPlayThroug());
+    this.audio.removeEventListener('canplaythrough', () =>
+      this.onCanPlayThroug(),
+    )
 
     this.audio.src = url
     this.index = 0
@@ -79,10 +84,9 @@ class Player {
     this.lrc._updateLrc(lrc)
 
     // 监听audio是否加载完毕
-    this.audio.addEventListener("canplaythrough", () => this.onCanPlayThroug());
+    this.audio.addEventListener('canplaythrough', () => this.onCanPlayThroug())
   }
   timeupdate() {
-    let processed = false;
     const updateTime = () => {
       const currentTime = +this.audio.currentTime.toFixed(2)
       const lrc = this.lrc._getLrc()
@@ -94,88 +98,93 @@ class Player {
       //0  5 >= 6  index = 0++ 在第一个检查完之后index就为1了，实际上应该等待过渡结束后
       //1  6 >= 7  index = 1++ 同理，实际上应该等待过渡结束后
       // 不++的话会导致一直进入这个判断。解决办法：
-      if(currentTime >= this.curLrcLine.time && !processed) {
-        processed = true; // 标记为已处理
-
+      if (currentTime >= this.curLrcLine.time) {
         console.log('this.index', this.index)
 
         curLineEl.classList.add('y-current-line')
-        this.curLineAllTextEl = curLineEl.children as HTMLCollectionOf<HTMLDivElement>
+        this.curLineAllTextEl =
+          curLineEl.children as HTMLCollectionOf<HTMLDivElement>
 
-        this.disposeAnimationProcess(0).then(() => {
+        this.disposeAnimationProcess(
+          this.curLineAllTextEl,
+          this.curLrcLine.yrc,
+          0,
+        ).then(() => {
           curLineEl.classList.remove('y-current-line')
-          this.index++
-          processed = false
         })
 
+        this.index++
       }
 
-      this._core.animationFrameId = requestAnimationFrame(updateTime);
-    };
+      this._core.animationFrameId = requestAnimationFrame(updateTime)
+    }
 
     // 取消上一次请求
-    cancelAnimationFrame(this._core.animationFrameId!);
+    cancelAnimationFrame(this._core.animationFrameId!)
 
     // 发起新的请求
-    this._core.animationFrameId = requestAnimationFrame(updateTime);
+    this._core.animationFrameId = requestAnimationFrame(updateTime)
   }
-  protected disposeAnimationProcess(index: number) {
+  protected disposeAnimationProcess(
+    els: HTMLCollectionOf<HTMLDivElement>,
+    yrcRule: Yrc[],
+    index: number,
+  ) {
     return new Promise((resolve, reject) => {
       const process = (index: number) => {
-        if(!this.curLineAllTextEl) {
-          return reject('disposeAnimationProcess：curLineAllTextEl元素为空')
+        if (!els) {
+          return reject(`disposeAnimationProcess：els元素为空, value: ${els}`)
         }
-        if(!this.curLrcLine) {
-          return reject('disposeAnimationProcess：curLrcLine为空')
+        if (!yrcRule) {
+          return reject(
+            `disposeAnimationProcess：yrcRule为空, value: ${yrcRule}`,
+          )
         }
-        if(index >= this.curLrcLine.yrc.length) {
+        if (index >= yrcRule.length) {
           return resolve('')
         }
         // 当前逐字元素
-        const textEl = this.curLineAllTextEl[index]
-        const curYrcRule = this.curLrcLine.yrc[index]
+        const textEl = els[index]
+        const curYrcRule = yrcRule[index]
         const glowYrc = curYrcRule.glowYrc
         // 处理辉光歌词
-        if(glowYrc) {
-          for(let o = 0; o < glowYrc.length; o++) {
-
-          }
+        if (glowYrc) {
+          for (let o = 0; o < glowYrc.length; o++) {}
         }
 
-        const [keyframes, options] = this.getLrcAnimationRule(curYrcRule.transition * 1000)
+        const [keyframes, options] = this.getLrcAnimationRule(
+          curYrcRule.transition * 1000,
+        )
         const animate = textEl.animate(keyframes, options)
-        textEl.animate([
-          {transform: 'translateY(0px)'},
-          {transform: 'translateY(-2px)'},
-        ],   {
-          duration: 700,
-          fill: "forwards",
-        })
-        this.curAnimation = animate
-        this.curAnimations.push(animate)
+        textEl.animate(
+          [{ transform: 'translateY(0px)' }, { transform: 'translateY(-2px)' }],
+          {
+            duration: 700,
+            fill: 'forwards',
+          },
+        )
 
         // 在动画完成后执行处理
         animate.finished.then(() => {
-          process(++index);
-        });
+          process(++index)
+        })
       }
       process(index)
     })
   }
-  protected getLrcAnimationRule(duration: number): [Keyframe[], KeyframeAnimationOptions] {
+  protected getLrcAnimationRule(
+    duration: number,
+  ): [Keyframe[], KeyframeAnimationOptions] {
     return [
-      [
-        {backgroundSize: '0% 100%'},
-        {backgroundSize: '100% 100%'},
-      ],
+      [{ backgroundSize: '0% 100%' }, { backgroundSize: '100% 100%' }],
       {
         duration,
-        fill: "forwards",
-      }
+        fill: 'forwards',
+      },
     ]
   }
   protected clearTimeupdate() {
-    cancelAnimationFrame(this._core.animationFrameId!);
+    cancelAnimationFrame(this._core.animationFrameId!)
   }
   protected getCurrentLrcLine() {
     const lrc = this.lrc._getLrc()
@@ -183,22 +192,22 @@ class Player {
 
     return lrc.find((item, index) => {
       // 获取第一行
-      if(index === 0 && currentTime <= item.time) {
+      if (index === 0 && currentTime <= item.time) {
         return item
       }
-      const nextItem = lrc[index+1]
+      const nextItem = lrc[index + 1]
       // 获取最后一行
-      if(!nextItem) {
+      if (!nextItem) {
         return item
       }
-      if(currentTime >= item.time && currentTime < nextItem.time) {
+      if (currentTime >= item.time && currentTime < nextItem.time) {
         return item
       }
     }) as LyricsLine
   }
   protected async onCanPlayThroug() {
     try {
-      await this.play();
+      await this.play()
     } catch (error) {
       Logger.error('更新歌词时播放失败：', error)
     }
